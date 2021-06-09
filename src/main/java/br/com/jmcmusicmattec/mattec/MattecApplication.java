80a35d45b9a934f741775f49896bbe882af1f4c9
jessemusic <jmc_cursos@hotmail.com>
2021-06-09 18:08:06 -0300

    Project created

---
+++ src/main/java/br/com/jmcmusicmattec/mattec/MattecApplication.java
@@ -0,0 +1,13 @@
+package br.com.jmcmusicmattec.mattec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MattecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MattecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jmcmusicmattec/mattec/MattecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jmcmusicmattec.mattec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MattecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
