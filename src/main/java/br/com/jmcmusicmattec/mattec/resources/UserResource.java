3cbf64431f62107d777639d1bd07cc22b97f9739
jessemusic <jmc_cursos@hotmail.com>
2021-06-10 11:20:51 -0300

    H2 database,test profile,JPA in memory

--- src/main/java/br/com/jmcmusicmattec/mattec/resources/UserResource.java
+++ src/main/java/br/com/jmcmusicmattec/mattec/resources/UserResource.java
@@ -13,7 +13,7 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Jesse", "jmc_cursos@hotmaikl.com", "9999999", "12345");
+		User u = new User(1L, "Jesse", "jmc_cursos@hotmail.com", "9999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
 	
