61d2c21e50589e172394b9614a3f95a6d8e43cf4
jessemusic <jmc_cursos@hotmail.com>
2021-06-13 23:37:47 -0300

    Exception handling-delete

--- src/main/java/br/com/jmcmusicmattec/mattec/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/jmcmusicmattec/mattec/resources/exceptions/ResourceExceptionHandler.java
@@ -4,12 +4,12 @@ import java.time.Instant;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.aspectj.weaver.StandardAnnotation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import br.com.jmcmusicmattec.mattec.services.exceptions.DatabaseException;
 import br.com.jmcmusicmattec.mattec.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -18,11 +18,21 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> resourceNotFound (ResourceNotFoundException e,
 			HttpServletRequest request){
-		String error = "Resource not found";
+		String error = "Resource not found ";
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 		
 	}
-
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> dataBase (DatabaseException e,
+			HttpServletRequest request){
+		String error = "DataBase Error ";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		
+	}
+ 
 }
